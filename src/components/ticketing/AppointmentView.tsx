5c6cbcec40de4be9529b5a699669801ec75d900e
geosync-ai <geosyncsurvey@gmail.com>
2025-04-27 06:41:03 +1000

    Ticketing System

--- src/components/ticketing/AppointmentView.tsx
+++ src/components/ticketing/AppointmentView.tsx
@@ -838,7 +838,7 @@ const AppointmentView: React.FC = () => {
               form="appointment-form" 
               className="bg-primary hover:bg-primary/90 text-white px-6 py-2 rounded-lg"
             >
-              {/* Update button text based on editing state */ 
+              {/* Update button text based on editing state */}
               {editingAppointment ? 'Save Changes' : 'Create Appointment'}
             </Button>
           </DialogFooter>
